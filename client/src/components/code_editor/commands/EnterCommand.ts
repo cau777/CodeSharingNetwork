8307983f4f7c91d38915181f58d269de1f78834d
cau777 <cauarinaldi451@gmail.com>
2021-09-23 13:41:49 -0300

    Changed EnterCommand.ts to scroll when pressing enter

--- client/src/components/code_editor/commands/EnterCommand.ts
+++ client/src/components/code_editor/commands/EnterCommand.ts
@@ -3,7 +3,7 @@ import React from "react";
 import {regexTestRange} from "../../../utils/StringUtils";
 import {CodeEditorOptions} from "../CodeEditorOptions";
 
-export class EnterCommand extends CodeEditorCommand{
+export class EnterCommand extends CodeEditorCommand {
     public constructor() {
         super(true, true);
     }
@@ -15,10 +15,11 @@ export class EnterCommand extends CodeEditorCommand{
     protected performAction(component: React.Component, e: React.KeyboardEvent<HTMLTextAreaElement>, options: CodeEditorOptions): void {
         let target = e.currentTarget;
         
+        // If there is ] ) or } right after the cursor, send this character to the next line
         if (regexTestRange(target.value, /^ *[})\]]/, target.selectionEnd)) {
             let level = super.calcIndentationLevel(target);
             let firstPart = "\n" + this.generateIndentation(level, options);
-            let secondPart = "\n" + this.generateIndentation(level-1, options);
+            let secondPart = "\n" + this.generateIndentation(level - 1, options);
             this.insertValue(target, firstPart);
             this.insertValue(target, secondPart);
             target.selectionStart -= secondPart.length;
@@ -26,6 +27,11 @@ export class EnterCommand extends CodeEditorCommand{
         } else {
             let indentation = this.generateIndentation(this.calcIndentationLevel(target), options);
             this.insertValue(target, "\n" + indentation);
+            
+        }
+        
+        if (target.scrollHeight !== 0) {
+            target.scrollBy(0, 23);
         }
     }
 }
