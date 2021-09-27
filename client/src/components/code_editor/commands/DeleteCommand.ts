53ce100cd35acdecb1a74210123e0b3acb819739
cau777 <cauarinaldi451@gmail.com>
2021-09-27 11:38:29 -0300

    Changed font to JetBrainsMono

--- client/src/components/code_editor/commands/DeleteCommand.ts
+++ client/src/components/code_editor/commands/DeleteCommand.ts
@@ -2,14 +2,14 @@ import {CodeEditorCommand} from "./CodeEditorCommand";
 import React from "react";
 import {CodeEditorOptions} from "../CodeEditorOptions";
 
-export class DeleteCommand extends CodeEditorCommand{
+export class DeleteCommand extends CodeEditorCommand {
     public canExecute(alt: boolean, ctrl: boolean, shift: boolean, key: string): boolean {
         return !alt && !ctrl && !shift && key === "Delete";
     }
     
     public async performAction(target: HTMLTextAreaElement, e: React.KeyboardEvent<HTMLTextAreaElement>, options: CodeEditorOptions): Promise<void> {
         if (target.selectionStart === target.selectionEnd) {
-            target.selectionEnd = Math.min(target.value.length , target.selectionEnd + 1);
+            target.selectionEnd = Math.min(target.value.length, target.selectionEnd + 1);
         }
         
         this.insertValue(target, "");
