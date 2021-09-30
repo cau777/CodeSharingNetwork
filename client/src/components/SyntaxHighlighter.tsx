47171ebd8d30b24d5c53a5e2384fa15b70b02a56
cau777 <cauarinaldi451@gmail.com>
2021-09-29 22:29:44 -0300

    Moved methods to SyntaxHighlighter.tsx and added escaping for string highlighting

---
+++ client/src/components/SyntaxHighlighter.tsx
@@ -0,0 +1,113 @@
+import {LanguageOptions} from "./code_editor/languages/LanguageOptions";
+import {escapeRegExp} from "../utils/StringUtils";
+
+export class SyntaxHighlighter {
+    private readonly keywordClass = "keyword";
+    private readonly numberClass = "number";
+    private readonly stringClass = "string";
+    private readonly commentClass = "comment";
+    private readonly options: LanguageOptions;
+    
+    public constructor(options: LanguageOptions) {
+        this.options = options;
+    }
+    
+    public highlight(text: string) {
+        let charColors: string[] = [];
+        
+        for (let j = 0; j < text.length; j++) {
+            charColors.push("");
+        }
+        
+        for (let keyword of this.options.keywords) {
+            SyntaxHighlighter.applyColor(text, charColors, "\\b" + escapeRegExp(keyword) + "\\b", this.keywordClass);
+        }
+        
+        for (let literal of this.options.literals) {
+            SyntaxHighlighter.applyColor(text, charColors, "\\b" + escapeRegExp(literal) + "\\b", this.keywordClass);
+        }
+        
+        if (this.options.numberHighlight) {
+            SyntaxHighlighter.applyColor(text, charColors, "\\b[_\\d]+\\b", this.numberClass);
+        }
+        
+        if (this.options.stringHighlight) {
+            let enclosedBy = "";
+            for (let i = 0; i < text.length; i++) {
+                let char = text.charAt(i);
+                let escaping = i !== 0 && text.charAt(i - 1) === "\\";
+                
+                if (enclosedBy === "") {
+                    if ((char === "\"" || char === "'") && !escaping) {
+                        enclosedBy = char;
+                        charColors[i] = this.stringClass;
+                    }
+                } else {
+                    charColors[i] = this.stringClass;
+                    if (char === enclosedBy && !escaping) {
+                        enclosedBy = "";
+                    }
+                }
+            }
+        }
+        
+        if (this.options.inlineComments) {
+            SyntaxHighlighter.applyColor(text, charColors, escapeRegExp(this.options.inlineComments) + ".*", this.commentClass);
+        }
+        
+        if (this.options.multilineComments) {
+            let regexStr = escapeRegExp(this.options.multilineComments.start) + "[\\s\\S]*?(" + escapeRegExp(this.options.multilineComments.end) + "|$)";
+            SyntaxHighlighter.applyColor(text, charColors, regexStr, this.commentClass);
+        }
+        
+        return charColors;
+    }
+    
+    public generateLines(text: string, charColors: string[]) {
+        let lineResult: JSX.Element[] = [];
+        let lines: JSX.Element[][] = [];
+        let currentColor = "";
+        let textBuffer = "";
+        let spanIndex = 0;
+        
+        for (let i = 0; i <= text.length; i++) {
+            let char = text.charAt(i);
+            
+            if (char === "\n" || i === text.length) {
+                if (textBuffer !== "")
+                    lineResult.push(<span key={spanIndex++} className={"code-" + currentColor}>{textBuffer}</span>);
+                
+                if (lineResult.length === 0) lineResult.push(<span key={spanIndex++}>{" "}</span>);
+                lines.push(lineResult);
+                lineResult = [];
+                textBuffer = "";
+                currentColor = "";
+            } else {
+                let color = charColors[i];
+                
+                if (currentColor !== color) {
+                    if (textBuffer !== "")
+                        lineResult.push(<span key={spanIndex++} className={"code-" + currentColor}>{textBuffer}</span>);
+                    currentColor = color;
+                    textBuffer = "";
+                }
+                
+                textBuffer += char;
+            }
+        }
+        
+        return lines;
+    }
+    
+    private static applyColor(text: string, charColors: string[], regexStr: string, colorCode: string) {
+        let matches = text.matchAll(new RegExp(regexStr, "g"));
+        for (let match of matches) {
+            let start = match.index;
+            if (start === undefined) continue;
+            
+            for (let groupIndex = 0; groupIndex < match[0].length; groupIndex++) {
+                charColors[start + groupIndex] = colorCode;
+            }
+        }
+    }
+}
--- client/src/components/code_editor/CodeEditorDisplay.tsx
+++ client/src/components/code_editor/CodeEditorDisplay.tsx
@@ -1,6 +1,6 @@
 import {Component} from "react";
 import {LanguageOptions} from "./languages/LanguageOptions";
-import {escapeRegExp} from "../../utils/StringUtils";
+import {SyntaxHighlighter} from "../SyntaxHighlighter";
 
 interface IProps {
     text: string;
@@ -10,101 +10,22 @@ interface IProps {
 
 export class CodeEditorDisplay extends Component<IProps, any> {
     public render() {
-        let keywordClass = "keyword";
-        let numberClass = "number";
-        let stringClass = "string";
-        let commentClass = "comment";
-        
-        let text = this.props.text;
-        let lines: JSX.Element[] = [];
-        let charColors: string[] = [];
         let options = this.props.language;
-        
-        for (let j = 0; j < text.length; j++) {
-            charColors.push("");
-        }
-        
-        for (let keyword of options.keywords) {
-            CodeEditorDisplay.applyColor(text, charColors, "\\b" + escapeRegExp(keyword) + "\\b", keywordClass);
-        }
-        
-        for (let literal of options.literals) {
-            CodeEditorDisplay.applyColor(text, charColors, "\\b" + escapeRegExp(literal) + "\\b", keywordClass);
-        }
-        
-        if (options.numberHighlight) {
-            CodeEditorDisplay.applyColor(text, charColors, "\\b[_\\d]+\\b", numberClass);
-        }
-        
-        if (options.stringHighlight) {
-            let enclosedBy = "";
-            for (let i = 0; i < text.length; i++) {
-                let char = text.charAt(i);
-                
-                if (enclosedBy === "") {
-                    if (char === "\"" || char === "'") {
-                        enclosedBy = char;
-                        charColors[i] = stringClass;
-                    }
-                } else {
-                    charColors[i] = stringClass;
-                    if (char === enclosedBy) {
-                        enclosedBy = "";
-                    }
-                }
-            }
-            CodeEditorDisplay.applyColor(text, charColors, "\"[^\"]*(\"|$)", stringClass);
-            CodeEditorDisplay.applyColor(text, charColors, "'[^']*('|$)", stringClass);
-        }
-        
-        if (options.inlineComments) {
-            CodeEditorDisplay.applyColor(text, charColors, escapeRegExp(options.inlineComments) + ".*", commentClass);
-        }
-        
-        if (options.multilineComments) {
-            let regexStr = escapeRegExp(options.multilineComments.start) + "[\\s\\S]*?(" + escapeRegExp(options.multilineComments.end) + "|$)";
-            CodeEditorDisplay.applyColor(text, charColors, regexStr, commentClass);
-        }
-        
-        let lineResult: JSX.Element[] = [];
+        let highlighter = new SyntaxHighlighter(options);
+        let text = this.props.text;
+        let charColors = highlighter.highlight(text);
+        let lines = highlighter.generateLines(text, charColors);
+        let tableRows: JSX.Element[] = [];
         let lineIndex = 0;
-        let currentColor = "";
-        let textBuffer = "";
-        let spanIndex = 0;
         
-        for (let i = 0; i <= text.length; i++) {
-            let char = text.charAt(i);
-            
-            if (char === "\n" || i === text.length) {
-                if (textBuffer !== "")
-                    lineResult.push(<span key={spanIndex++} className={"code-" + currentColor}>{textBuffer}</span>);
-                
-                if (lineResult.length === 0) lineResult.push(<span key={spanIndex++}>{" "}</span>);
-                
-                lines.push(
-                    <tr key={"line " + lineIndex}>
-                        <td className={this.props.selected === lineIndex ? "selected" : ""}>
-                            {lineResult}
-                        </td>
-                    </tr>
-                );
-                
-                lineIndex++;
-                lineResult = [];
-                textBuffer = "";
-                currentColor = "";
-            } else {
-                let color = charColors[i];
-                
-                if (currentColor !== color) {
-                    if (textBuffer !== "")
-                        lineResult.push(<span key={spanIndex++} className={"code-" + currentColor}>{textBuffer}</span>);
-                    currentColor = color;
-                    textBuffer = "";
-                }
-                
-                textBuffer += char;
-            }
+        for (let line of lines) {
+            tableRows.push(
+                <tr key={"line " + lineIndex}>
+                    <td className={this.props.selected === lineIndex ? "selected" : ""}>
+                        {line}
+                    </td>
+                </tr>);
+            lineIndex++;
         }
         
         return (
@@ -113,21 +34,9 @@ export class CodeEditorDisplay extends Component<IProps, any> {
                     <col/>
                 </colgroup>
                 <tbody>
-                {lines}
+                {tableRows}
                 </tbody>
             </table>
         );
-    }
-    
-    private static applyColor(text: string, charColors: string[], regexStr: string, colorCode: string) {
-        let matches = text.matchAll(new RegExp(regexStr, "g"));
-        for (let match of matches) {
-            let start = match.index;
-            if (start === undefined) continue;
-            
-            for (let groupIndex = 0; groupIndex < match[0].length; groupIndex++) {
-                charColors[start + groupIndex] = colorCode;
-            }
-        }
     }
 }
